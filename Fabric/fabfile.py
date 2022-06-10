c52f8421fb65cefa8e40213b3651c13bd43f872b
MatysZeZ <casyneu@gmail.com>
2022-06-10 16:52:33 +0200

    Fixed hosts in grafana task

--- Fabric/fabfile.py
+++ Fabric/fabfile.py
@@ -11,7 +11,7 @@ app_name = ''
 vm_host1 = 'username@ip_address'
 vm_host2 = 'username@ip_address'
 vm_host3 = 'username@ip_address'
-hosts = [vm_host1, vm_host2, vm_host3]
+
 ssh_path = '/path/to/keyfile/id_rsa'
 
 c = Connection(host=vm_host1,
@@ -40,6 +40,7 @@ def static_deploy_to_one_instance(c):
 
 @task
 def static_deploy_to_many_instances(c):
+   
    print(" Deploy static app to instance: " + str(hosts))
    repo_url = 'https://github.com/Matys98/my-cv.git'
    repo_dir = 'my-cv/cv'
@@ -63,7 +64,26 @@ def grafana_deploy_to_one_instance(c):
    copy_config_files(c)
    configure_solar(c)
 
+@task
+def grafana_deploy_to_many_instance(c):
+   hosts = [vm_host1, vm_host2]
+   db_host = vm_host3
+   db_name = 'inverter_power'
+   print(" Deploy solar panel app to instance: " + str(hosts) + " \nDeploy db to " + db_host)
+
+   c = Connection(host=db_host,
+       connect_kwargs={"key_filename": ssh_path})
+   download_influxdb(c, db_name)
+
+   for c in SerialGroup(hosts):
+      instal_apps(c)
+      copy_config_files(c)
+      configure_solar(c)
 
 @task
-def tiquet_deploy(c):
+def tiquet_deploy_to_one_instance(c):
+   c.run('uname -s')
+
+@task
+def tiquet_deploy_to_many_instance(c):
    c.run('uname -s')

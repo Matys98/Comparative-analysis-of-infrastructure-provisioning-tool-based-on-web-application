17e2bf471077ef1585dea8639d19a99378d5602b
MatysZeZ <casyneu@gmail.com>
2022-06-13 11:07:07 +0200

    Added fabric code for tiquet app

--- Fabric/fabfile.py
+++ Fabric/fabfile.py
@@ -82,8 +82,45 @@ def grafana_deploy_to_many_instance(c):
 
 @task
 def tiquet_deploy_to_one_instance(c):
-   c.run('uname -s')
+   print(" Deploy solar panel app to instance: " + vm_host1)
+   app_name = "tiquet"
+   instal_apps(c)
+   config_app(c)
+   config_db(c)
+   config_backend(c)
+   run_backend(c, app_name)
+   config_frontend(c)
+   run_frontend(c, app_name)
 
 @task
 def tiquet_deploy_to_many_instance(c):
-   c.run('uname -s')
+   fronend_host = vm_host1
+   backend_host = vm_host2
+   db_host = vm_host3
+   app_name = "tiquet"
+
+   print(" Deploy " + app_name + " frontend to instance:" + fronend_host)
+   print(" Deploy " + app_name + " backendend to instance:" + backend_host)
+   print(" Deploy " + app_name + " data base to instance:" + db_host)
+
+   # Connecting to Data base instance
+   c = Connection(host=db_host,
+       connect_kwargs={"key_filename": ssh_path})
+   instal_apps(c)
+   config_db(c)
+
+   # Connecting to Backend instance
+   c = Connection(host=backend_host,
+       connect_kwargs={"key_filename": ssh_path})
+   instal_apps(c)
+   config_app(c)
+   config_backend(c)
+   run_backend(c, app_name)
+
+   # Connecting to Frontend instance
+   c = Connection(host=fronend_host,
+       connect_kwargs={"key_filename": ssh_path})
+   instal_apps(c)
+   config_app(c)
+   config_frontend(c)
+   run_frontend(c, app_name)
--- Fabric/tiquet_module/tiquet.py
+++ Fabric/tiquet_module/tiquet.py
@@ -1,20 +1,100 @@
 from fabric import Connection, task, Config
 import getpass
 
-def clone_repo():
-    print("")
+def instal_apps(c):
+    c.run('apt-get update')
+    c.run('apt-get install curl -y')
+    c.run('apt install software-properties-common -y')
+    c.run('apt-get update')
 
-def config_frontend():
-    print("")
 
-def config_backend():
-    print("")
+def config_app(c):
+    c.run('apt-get install git -y')
+    c.run('cd ')
+    c.run('mkdir app')
+    c.run('cd app')
 
-def config_db():
-    print("")
+    c.run('git clone https://github.com/FLiotta/Tiquet.git')
 
-def run_frontend():
-    print("")
+    c.run('cd ')
+    c.run("sed -i 's/cffi==1.14.0/cffi==1.14.1/g' ./app/Tiquet/server/requirements.txt")
 
-def run_backend():
-    print("")
+def config_db(c):
+    c.run('curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg')
+    c.run('echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list')
+    c.run('apt-get install postgresql -y')
+    c.run('apt-get install postgresql-client-common -y')
+    c.run('apt-get install postgresql-client -y')
+
+    c.run('''
+        sudo bash -c "cat > /etc/postgresql/14/main/pg_hba.conf << EOF
+        # TYPE  DATABASE        USER            ADDRESS                 METHOD
+        local   all             all                                     trust
+        # IPv4 local connections:
+        host    all             all             127.0.0.1/32            trust
+        # IPv6 local connections:
+        host    all             all             ::1/128                 trust
+        # Allow replication connections from localhost, by a user with the
+        # replication privilege.
+        host    replication     all             127.0.0.1/32            trust
+        host    replication     all             ::1/128                 trust
+        local   all             all                                     trust
+        EOF
+        "
+    ''')
+
+    c.run('systemctl restart postgresql')
+    c.run('psql -U postgres -c "CREATE DATABASE tiquet"')
+
+def config_frontend(c):
+    c.run('apt-get install nodejs -y')
+    c.run('apt-get install npm -y')
+
+    c.run('apt-get install update')
+
+    c.run('cd ./app/Tiquet/client')
+
+    c.run('npm install')
+    c.run('npm audit fix')
+    c.run('npm install serve -g')
+    c.run('npm install pm2 -g ')
+
+
+def run_frontend(c, app_name):
+    c.run('npm run bundle')
+    c.run('pm2 start --name ' + app_name + '_frontend npm -- start')
+    c.run('cd ')
+
+def config_backend(c):
+    c.run('add-apt-repository ppa:deadsnakes/ppa -y')
+    c.run('apt-get install python3.8 -y')
+    c.run('apt-get install python3-pip -y')
+    c.run('apt-get install python3-virtualenv -y')
+
+    c.run('apt install libpq-dev -y')
+    c.run('apt install libffi-dev -y')
+
+    c.run('cd ./app/Tiquet/server')
+
+    c.run('virtualenv env')
+    c.run('source env/bin/activate')
+
+    c.run('echo requests==2.25.1 >> ./requirements.txt')
+    c.run('pip install -r ./requirements.txt')
+    c.run('''
+            sudo bash -c "cat > ./app/config.py << EOF
+            import os
+
+            DEBUG = True
+            CORS_HEADERS = 'Content-Type'
+            SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/tiquet'
+            SQLALCHEMY_TRACK_MODIFICATIONS = False
+            EOF
+            "
+    ''')
+
+def run_backend(c, app_name):
+    c.run('python3 create_tables.py')
+    c.run('pm2 start run.py --interpreter python3 --name '+ app_name + '_backend')
+    c.run('deactivate')
+    c.run('cd ')

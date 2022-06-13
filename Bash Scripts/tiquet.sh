40618c5e41d42e345fae2bf70d5b3e90a7b28fb1
MatysZeZ <casyneu@gmail.com>
2022-06-13 20:58:32 +0200

    Updated bash scripts and gitignore

--- Bash Scripts/tiquet.sh
+++ Bash Scripts/tiquet.sh
@@ -113,6 +113,7 @@ DEBUG = True
 CORS_HEADERS = 'Content-Type'
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/tiquet'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+EOF
 "
 # 4. Configure DB
 python3 create_tables.py

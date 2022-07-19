0bc283693363dcfe464a7bf2389b06d1e390ae0f
MatysZeZ <casyneu@gmail.com>
2022-07-19 22:33:16 +0000

    Updated deploymend files

---
+++ Bash Scripts/bash_deployment/tiquet.sh
@@ -0,0 +1,4 @@
+scp ../tiquet.sh  vagrant@192.168.56.21:/home/vagrant/script.sh
+scp -r ../Tiquet/  vagrant@192.168.56.21:/home/vagrant/Tiquet/
+ssh -i ./home/user/.ssh/id_rsa vagrant@192.168.56.21 'chmod u+x script.sh'
+ssh -i ./home/user/.ssh/id_rsa vagrant@192.168.56.21 './script.sh'

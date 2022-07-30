ead06d0a9ebc7662334f357e64e74f9708a2dd1b
MatysZeZ <casyneu@gmail.com>
2022-07-30 09:49:21 +0000

    Fixed path for multiple bash

--- Bash Scripts/bash_deployment/static/multi_staticwebapp.sh
+++ Bash Scripts/bash_deployment/static/multi_staticwebapp.sh
@@ -1,11 +1,11 @@
-scp ../staticwebapp.sh  vagrant@192.168.56.21:/home/vagrant/script.sh
+scp ../../staticwebapp.sh  vagrant@192.168.56.21:/home/vagrant/script.sh
 ssh -i ./home/user/.ssh/id_rsa vagrant@192.168.56.21 'chmod u+x script.sh'
 ssh -i ./home/user/.ssh/id_rsa vagrant@192.168.56.21 ./script.sh
 
-scp ../staticwebapp.sh  vagrant@192.168.56.22:/home/vagrant/script.sh
+scp ../../staticwebapp.sh  vagrant@192.168.56.22:/home/vagrant/script.sh
 ssh -i ./home/user/.ssh/id_rsa vagrant@192.168.56.22 'chmod u+x script.sh'
 ssh -i ./home/user/.ssh/id_rsa vagrant@192.168.56.22 ./script.sh
 
-scp ../staticwebapp.sh  vagrant@192.168.56.23:/home/vagrant/script.sh
+scp ../../staticwebapp.sh  vagrant@192.168.56.23:/home/vagrant/script.sh
 ssh -i ./home/user/.ssh/id_rsa vagrant@192.168.56.23 'chmod u+x script.sh'
 ssh -i ./home/user/.ssh/id_rsa vagrant@192.168.56.23 ./script.sh

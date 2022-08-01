870c0e40cd30bb0ea2965dcbd052a9ef1837994f
MatysZeZ <casyneu@gmail.com>
2022-08-01 22:13:41 +0000

    Fixed all problems

---
+++ Vagrant/reset_ssh.sh
@@ -0,0 +1,3 @@
+ssh-keygen -f "/home/matys/.ssh/known_hosts" -R "192.168.56.21"
+ssh-keygen -f "/home/matys/.ssh/known_hosts" -R "192.168.56.22"
+ssh-keygen -f "/home/matys/.ssh/known_hosts" -R "192.168.56.23"

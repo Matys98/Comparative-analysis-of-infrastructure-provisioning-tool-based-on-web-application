94ab974fb0ad65c4ce6ccec51c61659a4d79460e
MatysZeZ <casyneu@gmail.com>
2022-08-03 16:28:28 +0200

    Update restart_ssh.sh

--- Vagrant/reset_ssh.sh
+++ Vagrant/reset_ssh.sh
@@ -1,3 +1,7 @@
 ssh-keygen -f "/home/matys/.ssh/known_hosts" -R "192.168.56.21"
 ssh-keygen -f "/home/matys/.ssh/known_hosts" -R "192.168.56.22"
 ssh-keygen -f "/home/matys/.ssh/known_hosts" -R "192.168.56.23"
+
+ssh-keyscan -H vagrant@192.168.56.21 >> /home/matys/.ssh/known_hosts
+ssh-keyscan -H vagrant@192.168.56.22 >> /home/matys/.ssh/known_hosts
+ssh-keyscan -H vagrant@192.168.56.23 >> /home/matys/.ssh/known_hosts
